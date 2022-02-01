8f0951ab897c95831ca73158fd5a8cad99703806
AbrilPComparan <96149811+AbrilPComparan@users.noreply.github.com>
2022-01-31 18:54:27 -0600

    Se suben practicas con Variables

---
+++ Variables/src/Main.java
@@ -0,0 +1,13 @@
+package variables;
+
+public class Main {
+	public static void main (String args[]) {
+		
+		String nombre = "Abril";
+		int edad = 24;
+		double estatura = 1.64;
+		
+		System.out.println("Hola "+ nombre + ", tienes "+edad+" años y mides "+ estatura);
+	}
+}
+//
